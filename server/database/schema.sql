6823f3b2dd6d4c23ee9d994a823eb168693fdea7
Adeline <149504531+kna1703@users.noreply.github.com>
2024-07-17 16:18:28 +0200

    add bdd game

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,36 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE user(
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  pseudo VARCHAR(30) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  hashed_password VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE game (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  tagline VARCHAR(255) NOT NULL,
+  overview TEXT NOT NULL,
+  illustration VARCHAR(255) NOT NULL, 
+  background VARCHAR(255),
+  player VARCHAR(10) NOT NULL,
+  duration INT NOT NULL,
+  age VARCHAR(4) NOT NULL
 );
+
+-- CREATE TABLE review (
+--   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--   notation INT UNSIGNED NOT NULL CHECK (notation >= 1 AND notation <= 10),
+--   comment TEXT NOT NULL,
+--   game_id INT NOT NULL UNSIGNED,
+--   user_id INT NOT NULL UNSIGNED,
+--   FOREIGN KEY (game_id) REFERENCES game(id),
+--   FOREIGN KEY (user_id) REFERENCES user(id)
+-- );
+
+-- CREATE TABLE game_user (
+--   game_id INT NOT NULL UNSIGNED,
+--   user_id INT NOT NULL UNSIGNED,
+--   PRIMARY KEY (game_id, user_id),
+--   FOREIGN KEY (game_id) REFERENCES game(id),
+--   FOREIGN KEY (user_id) REFERENCES user(id)
+-- );
