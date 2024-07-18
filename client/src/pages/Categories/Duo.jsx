7e1d5cf59ba9233b398a2ffbd8552004d1ac1870
Adeline <149504531+kna1703@users.noreply.github.com>
2024-07-18 17:27:40 +0200

    fix prettier

--- client/src/pages/Categories/Duo.jsx
+++ client/src/pages/Categories/Duo.jsx
@@ -4,6 +4,7 @@ import styles from "./Categories.module.css";
 function Duo() {
   const allGames = useLoaderData();
   const duoGames = allGames.filter((game) => game.duo);
+  const multiGames = allGames.filter((game) => !game.duo);
   return (
     <div className={styles.contain}>
       <div className={styles.allBloc}>
@@ -20,6 +21,21 @@ function Duo() {
                 <p className={styles.gameName}>{game.name}</p>
               </div>
             ))}
+          </div>
+          <div className={styles.bloc}>
+            <p className={styles.cat}>MULTI-JOUEURS</p>
+            <div className={styles.allGames}>
+              {multiGames.map((game) => (
+                <div key={game.id} className={styles.listGames}>
+                  <img
+                    src={game.illustration}
+                    alt={game.name}
+                    className={styles.image}
+                  />
+                  <p className={styles.gameName}>{game.name}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
